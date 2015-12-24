0a17472c5d1a084aa617f46889988efa327e8c48
Mraqfrod <mraqfrod@163.com>
2015-12-24 19:47:19 +0800

    测试提交

--- app/src/main/java/explodeview/mraqfrod/com/myexplodeview/MainActivity.java
+++ app/src/main/java/explodeview/mraqfrod/com/myexplodeview/MainActivity.java
@@ -3,6 +3,9 @@ package explodeview.mraqfrod.com.myexplodeview;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ *  自己学习用的第一个项目
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
